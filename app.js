e32b11d0de1f0201c67c88b1afbe5139f63c3114
David Woolf <davidjohnwoolf@gmail.com>
2015-10-10 16:34:29 -0700

    add semicolon to app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var mongoose = require('mongoose');
-var flash = require('connect-flash')
+var flash = require('connect-flash');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
